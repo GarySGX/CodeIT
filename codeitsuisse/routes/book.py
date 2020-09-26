457e1e79f2c1ba6b9acaabca768eb8bef423a75b
GarySGX <shenguangxu1009@gmail.com>
2020-09-26 18:26:31 +0800

    change of books

--- codeitsuisse/routes/book.py
+++ codeitsuisse/routes/book.py
@@ -6,21 +6,22 @@ from codeitsuisse import app
 logger = logging.getLogger(__name__)
 
 @app.route('/olympiad-of-babylon', methods=['POST'])
-def Number(books,days):
+def olympiad():
     result = {'optimalNumberOfBooks':counter}
     books = request.get_json();
-    books.sort()
-    days.sort(reverse=True)
-    bookI=0
-    bookJ=len(books)-1
-    counter=0
-    for i in days:
-        while i>books[bookJ]:
-            i-=books[bookJ]
-            bookJ-=1
-            counter+=1
-        while i>books[bookI]:
-            i=books[bookI]
-            bookI+=1
-            counter+=1
+    def Number(books,days):
+        books.sort()
+        days.sort(reverse=True)
+        bookI=0
+        bookJ=len(books)-1
+        counter=0
+        for i in days:
+            while i>books[bookJ]:
+                i-=books[bookJ]
+                bookJ-=1
+                counter+=1
+            while i>books[bookI]:
+                i=books[bookI]
+                bookI+=1
+                counter+=1
     return json.dumps(result)
